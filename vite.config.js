9d7c38135430107f20ea9d2a0b38fb6aaefcbfd3
steaqygraphics <steaqygraphics@gmail.com>
2025-10-31 13:37:37 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Pastikan plugin @vitejs/plugin-react dimuat
-export default defineConfig({
-  plugins: [react()],
-  // Tidak ada konfigurasi build atau root kustom.
-  // Vite akan otomatis menggunakan index.html dan dist.
-});
