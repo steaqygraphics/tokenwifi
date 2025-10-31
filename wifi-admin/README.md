82a024ddb05dc55a1347e224e0f6d3e49fb033f1
steaqygraphics <steaqygraphics@gmail.com>
2025-10-31 15:08:12 +0700

    Delete wifi-admin directory

--- wifi-admin/README.md
+++
@@ -1,7 +0,0 @@
-# WiFi Admin Dashboard
-
-Project dashboard untuk mesin wifi berbasis React + Firebase + Vite.
-
-## Cara menjalankan
-npm install  
-npm run dev
--- wifi-admin/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="p-6 text-center font-bold text-xl">
-      WiFi Admin Dashboard
-    </div>
-  );
-}
